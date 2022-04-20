3b470ccf403188e76d576009fc57a2ca1ee3a595
Vladimir Kluchenkov <vkluchenkov@gmail.com>
2022-04-20 16:56:16 +0200

    fet users controller error handling

--- controllers/users.js
+++ controllers/users.js
@@ -1,46 +1,55 @@
+const { handleError } = require('../errors/handleError');
+const NotFoundError = require('../errors/NotFoundError');
 const User = require('../models/user');
 
-module.exports.getUsers = (req, res) => {
-  User.find({})
-    .then((users) => res.send({ data: users }))
-    .catch((err) => res.status(500).send({ message: err.message })); // обработка ошибок
+module.exports.getUsers = async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users) res.send(users);
+    else throw new NotFoundError('Пользователей не найдено');
+  } catch (err) {
+    handleError({ err, res });
+  }
 };
 
-module.exports.getUser = (req, res) => {
-  User.findById(req.params.id)
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res
-      .status(500)
-      .send({ message: 'Произошла ошибка при запросе пользователя' })); // обработка ошибок
+module.exports.getUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (user) res.send(user);
+    else throw new NotFoundError('Пользователь не найден');
+  } catch (err) {
+    handleError({ err, res });
+  }
 };
 
-module.exports.createUser = (req, res) => {
+module.exports.createUser = async (req, res) => {
   const { name, about, avatar } = req.body;
-
-  User.create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
-    .catch(() => res
-      .status(500)
-      .send({ message: 'Произошла ошибка при создании пользователя' })); // обработка ошибок
+  try {
+    const user = await User.create({ name, about, avatar });
+    res.send(user);
+  } catch (err) {
+    handleError({ err, res });
+  }
 };
 
-module.exports.updateMe = (req, res) => {
-  console.log(req.body);
+module.exports.updateMe = async (req, res) => {
   const { name, about } = req.body;
-
-  User.findByIdAndUpdate(req.user._id, { name, about })
-    .then((user) => res.send({ data: user }))
-    .catch(() => res
-      .status(500)
-      .send({ message: 'Произошла ошибка при обновлении пользователя' })); // обработка ошибок
+  try {
+    const user = await User.findByIdAndUpdate(req.user._id, { name, about }, { new: true });
+    if (user) res.send(user);
+    else throw new NotFoundError('Пользователь не найден');
+  } catch (err) {
+    handleError({ err, res });
+  }
 };
 
-module.exports.updateMeAvatar = (req, res) => {
+module.exports.updateMeAvatar = async (req, res) => {
   const { avatar } = req.body;
-
-  User.findByIdAndUpdate(req.user._id, { avatar })
-    .then((user) => res.send({ data: user }))
-    .catch(() => res
-      .status(500)
-      .send({ message: 'Произошла ошибка при обновлении аватара' })); // обработка ошибок
+  try {
+    const user = await User.findByIdAndUpdate(req.user._id, { avatar }, { new: true });
+    if (user) res.send(user);
+    else throw new NotFoundError('Пользователь не найден');
+  } catch (err) {
+    handleError({ err, res });
+  }
 };
